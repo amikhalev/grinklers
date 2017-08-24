aab8b5c1d2a87122d27175c24537ad805c269221
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-08-24 00:09:40 -0600

    Improved server cleanup

--- server/server.go
+++ server/server.go
@@ -21,7 +21,6 @@ func main() {
 	if err != nil {
 		log.WithError(err).Fatalf("error initializing sections")
 	}
-	defer g.RpioSectionCleanup()
 
 	config, err := g.LoadConfig()
 	if err != nil {
@@ -42,34 +41,39 @@ func main() {
 	updater := g.NewMQTTUpdater(&config, secRunner)
 
 	log.Debug("initializing sections and programs")
-	for i := range sections {
-		sections[i].SetState(false)
+
+	stopAll := func() {
+		for i := range sections {
+			sections[i].SetState(false)
+		}
 	}
+	stopAll()
+
 	for i := range programs {
 		programs[i].Start(secRunner, &waitGroup)
 	}
+
 	log.WithFields(log.Fields{
 		"lenSections": len(sections), "lenPrograms": len(programs),
 	}).Info("initialized sections and programs")
 
 	api := g.NewMQTTApi(&config, secRunner)
 	api.Start()
-	defer api.Stop()
 
 	updater.Start(api)
 	updater.UpdateSections()
 	updater.UpdatePrograms()
-	defer updater.Stop()
 
 	<-sigc
 
 	log.Info("cleaning up...")
+	updater.Stop()
+	api.Stop()
 	for i := range programs {
 		programs[i].Quit()
 	}
 	secRunner.Quit()
 	waitGroup.Wait()
-	for i := range sections {
-		sections[i].SetState(false)
-	}
+	stopAll()
+	g.RpioSectionCleanup()
 }
